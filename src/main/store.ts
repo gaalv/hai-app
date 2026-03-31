096bde995fbcd8558d0ba31ba9447532e4a9d085
gaalv <gaalv.dev@gmail.com>
2026-03-30 23:00:11 -0300

    chore: setup electron-store with VaultConfig schema

---
+++ src/main/store.ts
@@ -0,0 +1,18 @@
+import Store from 'electron-store'
+import type { VaultConfig } from '../renderer/src/types/vault'
+import type { SyncConfig } from '../renderer/src/types/sync'
+
+interface StoreSchema {
+  vaultConfig: VaultConfig | null
+  syncConfig: SyncConfig | null
+}
+
+const store = new Store<StoreSchema>({
+  name: 'muta-notes-config',
+  defaults: {
+    vaultConfig: null,
+    syncConfig: null
+  }
+})
+
+export default store
