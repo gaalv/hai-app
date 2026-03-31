cb82a2ecb3b340b4024911f7ec8baac0cdb4d2e2
gaalv <gaalv.dev@gmail.com>
2026-03-30 23:00:11 -0300

    chore: configure electron-builder for distribution

---
+++ electron-builder.config.ts
@@ -0,0 +1,29 @@
+import type { Configuration } from 'electron-builder'
+
+const config: Configuration = {
+  appId: 'com.gaalv.muta-notes',
+  productName: 'Muta Notes',
+  copyright: 'Copyright © 2025 gaalv',
+  directories: {
+    buildResources: 'resources',
+    output: 'dist'
+  },
+  files: ['out/**/*'],
+  mac: {
+    target: [{ target: 'dmg', arch: ['x64', 'arm64'] }],
+    category: 'public.app-category.productivity'
+  },
+  win: {
+    target: [{ target: 'nsis', arch: ['x64'] }]
+  },
+  linux: {
+    target: [{ target: 'AppImage', arch: ['x64'] }],
+    category: 'Office'
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true
+  }
+}
+
+export default config
